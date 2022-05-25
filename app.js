6e16ba2743b7531176b3506751aa9ad12bc4d5c5
Ajay Maurya <97455068+Ajay-Maury@users.noreply.github.com>
2022-05-25 18:16:41 +0530

    done

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const connect = require("./config/db");
 const shortId = require("shortid")
 const createHttpError = require("http-errors")
 const mongoose = require("mongoose")
 const path = require("path")
 const ShortUrl = require("./models/url.model");
+require("dotenv").config();
 
 
 const app = express()
@@ -11,10 +13,10 @@ app.use(express.static(path.join(__dirname, "public")))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-mongoose.connect(
-  "mongodb+srv://pata_nahi:bhool_gaya@cluster0.lsvn5.mongodb.net/url-shortner?retryWrites=true&w=majority"
-).then(() => console.log("MongoDB Connected"))
-.catch((error)=>console.log("Error Connecting : ",error))
+mongoose
+  .connect("mongodb+srv://pata_nahi:bhool_gaya@cluster0.lsvn5.mongodb.net/url-shortner?retryWrites=true&w=majority")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((error) => console.log("Error Connecting : ", error));
 
 app.set("view engine","ejs")
 app.get("/", async (req, res, next) => {
@@ -30,8 +32,8 @@ app.post("/", async (req, res, next) => {
         const urlExists = await ShortUrl.findOne({ url });
         if (urlExists) {
             res.render("index", {
-            //   short_url: `http://localhost:5000/${urlExists.shortId}`,
-              short_url: `${req.hostname}/${urlExists.shortId}`,
+              short_url: `http://localhost:5000/${urlExists.shortId}`,
+            //   short_url: `${req.hostname}/${urlExists.shortId}`,
             });
         }
 
@@ -39,8 +41,8 @@ app.post("/", async (req, res, next) => {
 
         const result = await shortUrl.save()
         res.render("index", {
-        //   short_url: `http://localhost:4000/${result.shortId}`
-          short_url: `${req.hostname}/${result.shortId}`
+          short_url: `http://localhost:4000/${result.shortId}`
+        //   short_url: `${req.hostname}/${result.shortId}`
         });
     }
     catch (error) {
@@ -70,4 +72,12 @@ app.use((err, req, res, next) => {
     res.render("index",{error:err.message})
 })
 
-app.listen(4000,()=>console.log("Listining at 4000"))
+// app.listen(4000, async (req, res) => {
+//   try {
+//     await connect();
+//     console.log("Listening at 4000");
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// });
+app.listen(4000, () => console.log("Listining at 4000"));
